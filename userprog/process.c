02072fdc354b3148813ad75ad0344f10a71df9c9
HyunggiPark <phg9587@naver.com>
2022-12-28 23:17:03 +0900

    fix: 무한루프 대신에 딜레이 함수로 사용

--- userprog/process.c
+++ userprog/process.c
@@ -213,9 +213,11 @@ process_wait (tid_t child_tid UNUSED) {
 	/* XXX: Hint) The pintos exit if process_wait (initd), we recommend you
 	 * XXX:       to add infinite loop here before
 	 * XXX:       implementing the process_wait. */
-	while (1) {
+	// while (1) {
+	// 	;
+	// }
+	for(int i = 0; i < 100000000; i++)
 		;
-	}
 	
 	return -1;
 }
