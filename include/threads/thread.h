94f5b7e91ae55d1962a94459cef60725c4c8d141
HyunggiPark <phg9587@naver.com>
2022-12-28 17:58:49 +0900

    feat: exit_status 관리를 위해 struct thread 에 exit_status 추가

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -108,6 +108,8 @@ struct thread {
 	struct supplemental_page_table spt;
 #endif
 
+	int exit_status;
+	
 	/* Owned by thread.c. */
 	struct intr_frame tf;               /* Information for switching */
 	unsigned magic;                     /* Detects stack overflow. */
