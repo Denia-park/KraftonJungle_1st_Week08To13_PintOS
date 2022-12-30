2ac00d53237d39cc44bca66ba17d197de47531e5
HyunggiPark <phg9587@naver.com>
2022-12-30 19:38:13 +0900

    fix: tests/userprog/no-vm/multi-oom 해결을 위해서 코드 수정

--- userprog/process.c
+++ userprog/process.c
@@ -107,9 +107,7 @@ process_fork (const char *name, struct intr_frame *if_ UNUSED) {
 	/* --- Project 2: system call --- */
 	struct thread *child = get_child(tid);
 	sema_down(&child->fork_sema);
-	if (child->exit_status == -1) {
-		return TID_ERROR;
-	}
+
 	return tid;
 }
 
